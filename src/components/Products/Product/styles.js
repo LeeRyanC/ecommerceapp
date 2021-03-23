0ef75a339b015ad35c7749b0c3a3179a271acc29
Ryan Lee <UltraLord@Ryans-MacBook-Pro-2.local>
2021-03-22 21:03:32 -0700

    cart features

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -14,6 +14,6 @@ export default makeStyles(() => ({
     },
     cardContent: {
         display: 'flex',
-        justifyContent: 'spac-between',
+        justifyContent: 'space-between',
     },
 }))
